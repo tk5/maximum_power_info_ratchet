ede214ce78a6028d774592d0e3470f693ac46554
jnlucero96 <jnlucero96@users.noreply.github.com>
2020-11-04 16:57:35 -0800

    Cleaning up and adding comments.

--- src/power_vs_frequency_numeric/ss_derive.py
+++ src/power_vs_frequency_numeric/ss_derive.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+# @author: Joseph N. E. Lucero
 
 from numpy import (
-    heaviside, exp as nexp, sqrt as nsqrt, pi as npi, finfo,
+    heaviside, exp, sqrt, pi, finfo,
     linspace, logspace, where, abs as nabs
     )
 from scipy.integrate import trapz
@@ -9,32 +10,44 @@ from scipy.integrate import trapz
 from scipy.sparse import csc_matrix
 from scipy.sparse.linalg import eigs
 
-from os import environ, mkdir
+from os import environ, makedirs
 from os.path import isdir
 
-from pltconfig import *
+
+def get_params():
+
+    """Set the parameters here."""
+
+    delta_g = 0.8
+    time_pow_low = 1.0
+    time_pow_up = -3.0
+    nscan = 40
+
+    return [delta_g, time_pow_low, time_pow_up, nscan]
 
 
 def N_distr(x, mu, sigma2):
     """Return the normal distribution with mean mu and variance sigma2."""
-    return nexp(-0.5*((x-mu)**2)/sigma2)/nsqrt(2.0*npi*sigma2)
+    return exp(-0.5*((x-mu)**2)/sigma2)/sqrt(2.0*pi*sigma2)
 
 
 def compute_p_r_rr(r, rr, delta_g, dt):
+    """Return the propagator for x_{n^{+}}^{r}."""
     return (
         heaviside(-rr[None, :], 0.0)*N_distr(
-            r[:, None], (rr[None, :]+delta_g)*nexp(-dt) - delta_g, 1.0-nexp(-2.0*dt)
+            r[:, None], (rr[None, :]+delta_g)*exp(-dt) - delta_g, 1.0-exp(-2.0*dt)
             )
         + heaviside(rr[None, :], 0.0)*N_distr(
-            r[:, None], -delta_g - (rr[None, :]-delta_g)*nexp(-dt), 1.0-nexp(-2.0*dt)
+            r[:, None], -delta_g - (rr[None, :]-delta_g)*exp(-dt), 1.0-exp(-2.0*dt)
             )
         )*nabs(r[1]-r[0])
 
 
 def compute_p_s_ss(s, ss, delta_g, dt):
+    """Return the propagator for x_{n}^{r}."""
     return heaviside(-s[:, None], 0.0)*(
-        N_distr(s[:, None], delta_g - (ss[None, :]+delta_g)*nexp(-dt), 1.0-nexp(-2.0*dt))
-        + N_distr(s[:, None], (ss[None, :]+delta_g)*nexp(-dt) - delta_g, 1.0-nexp(-2.0*dt))
+        N_distr(s[:, None], delta_g - (ss[None, :]+delta_g)*exp(-dt), 1.0-exp(-2.0*dt))
+        + N_distr(s[:, None], (ss[None, :]+delta_g)*exp(-dt) - delta_g, 1.0-exp(-2.0*dt))
     )*nabs(s[1]-s[0])
 
 
@@ -65,15 +78,19 @@ def find_steady_states(r, rr, delta_g, dt):
 
 def compute_means(target_dir):
 
-    delta_g = 0.8
-    low_limit = -20.0
-    up_limit = 20.0
-    ngrid = 2e3
-    grid_from = linspace(low_limit, up_limit, int(ngrid))
-    grid_to = linspace(low_limit, up_limit, int(ngrid))
+    """Run the calculation that gives you the steady-state
+    average power as a function of sampling time."""
 
-    nscan = 1
-    times = logspace(1.0, -3.0, nscan)
+    [delta_g, time_pow_low, time_pow_up, nscan] = get_params()
+
+    # set up the grid over to discretize equations over
+    LOW_LIMIT = -20.0
+    UP_LIMIT = 20.0
+    NGRID = 2e3
+    grid_from = linspace(LOW_LIMIT, UP_LIMIT, int(NGRID))
+    grid_to = linspace(LOW_LIMIT, UP_LIMIT, int(NGRID))
+
+    times = logspace(time_pow_low, time_pow_up, nscan)
 
     target_file = f"/ref_deltag_{delta_g:.2f}_outfile.txt"
 
@@ -94,12 +111,12 @@ def compute_means(target_dir):
 
 if __name__ == "__main__":
 
-    target_repo = "/Users/jlucero/data_dir/"
+    target_repo = "./data_dir/"
     today_dir = environ["TODAY"]
 
     target_dir = target_repo + "/" + today_dir + "/"
     if not isdir(target_dir):
         print("Target directory doesn't exist. Making it now.")
-        mkdir(target_dir)
+        makedirs(target_dir)
 
     compute_means(target_dir)
